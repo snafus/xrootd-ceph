f58cf56aaca0a29f59ed87dee0575a4027397009
snafus <james.walder@gmail.com>
2022-04-04 10:19:16 +0100

    Reduce logging
    Remove extraneous logging messages

--- src/XrdCeph/XrdCephPosix.cc
+++ src/XrdCeph/XrdCephPosix.cc
@@ -400,11 +400,11 @@ void translateFileName(std::string &physName, std::string logName){
       logwrapper((char*)"ceph_namelib : failed to translate %s using namelib plugin, using it as is", logName.c_str());
       physName = logName;
     } else {
-      logwrapper((char*)"ceph_namelib : translated %s to %s", logName.c_str(), physCName);
+      //logwrapper((char*)"ceph_namelib : translated %s to %s", logName.c_str(), physCName);
       physName = physCName;
     }
   } else {
-      logwrapper((char*)"ceph_namelib : No mapping done");
+    //logwrapper((char*)"ceph_namelib : No mapping done");
     physName = logName;
   }
 }
